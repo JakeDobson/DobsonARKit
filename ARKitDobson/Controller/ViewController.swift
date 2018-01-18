ae0b1c3c56fac2685d8647905ffbf6aaaefc7d2c
Jake Dobson <jake.dobson@gmail.com>
2018-01-18 12:17:38 -0800

    so the box falls and hits the plane or pyramid, but it boggles me as to WHY -- bitMask issue?

--- ARKitDobson/Controller/ViewController.swift
+++ ARKitDobson/Controller/ViewController.swift
@@ -5,16 +5,13 @@
 import UIKit
 import SceneKit
 import ARKit
-enum BoxStatus {
-	case added
-	case notAdded
-}
-//enum for category bit mask of physics bodies
+//enum for bit mask
 enum BodyType: Int {
 	case box = 1
-	case plane = 2
+	case pyramid = 2
+	case plane = 3
 }
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate {
 	//outlets
     @IBOutlet var sceneView: ARSCNView!
 	//globals
@@ -33,6 +30,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		self.view.addSubview(self.sceneView)
 		// Set the view's delegate
 		sceneView.delegate = self
+		//subscribe to physics contact delegate
+		self.sceneView.scene.physicsWorld.contactDelegate = self
 		//show statistics such as fps and timing information
 		sceneView.showsStatistics = true
 		//create new scene
@@ -43,9 +42,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
 		sceneView.addGestureRecognizer(tapGestureRecognizer)
 	}
-//MARK: helper funcs
-    //private var boxStatus: BoxStatus = .notAdded
-    
+	//MARK: helper funcs
 	@objc func tapped(recognizer: UIGestureRecognizer) {
 		let scnView = recognizer.view as! ARSCNView
 		let touchLocation = recognizer.location(in: scnView)
@@ -56,43 +53,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 			addBox(hitResult: hitResult)
 		}
 	}
-        
-//	private func nodeForScene(sceneName: String, nodeName: String) -> SCNNode? {
-//		let scn = SCNScene(named: sceneName)!
-//		return scn.rootNode.childNode(withName: nodeName, recursively: true)
-//	}
-
 	private func addBox(hitResult: ARHitTestResult) {
-		let boxGeometry = SCNBox(width: 0.1,
-									 height: 0.1,
-									 length: 0.1,
-									 chamferRadius: 0)
+		let boxGeometry = SCNBox(width:  0.1,
+								 height: 0.1,
+								 length: 0.1,
+								 chamferRadius: 0)
 		let material = SCNMaterial()
-		material.diffuse.contents = UIColor(red: .random(),
-											green: .random(),
-											blue: .random(),
-											alpha: 1.0)
+		material.diffuse.contents = UIColor(red: 	.random(),
+											green: 	.random(),
+											blue: 	.random(),
+											alpha: 	1.0)
 		boxGeometry.materials = [material]
 		let boxNode = SCNNode(geometry: boxGeometry)
 		//adding physics body, a box already has a shape, so nil is fine
 		boxNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
 		//set bitMask on boxNode, enabling objects with diff categoryBitMasks to collide w/ each other
-		boxNode.physicsBody?.categoryBitMask = BodyType.box.rawValue
+		boxNode.physicsBody?.categoryBitMask = BodyType.plane.rawValue | BodyType.box.rawValue
 		boxNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,
-										  hitResult.worldTransform.columns.3.y + 0.3,
-										  hitResult.worldTransform.columns.3.z)
-			self.sceneView.scene.rootNode.addChildNode(boxNode)
-//		}
+									  hitResult.worldTransform.columns.3.y + 0.3,
+									  hitResult.worldTransform.columns.3.z)
+		self.sceneView.scene.rootNode.addChildNode(boxNode)
 	}
-    
-//    private func addTaxi() {
-//		if let taxiNode = nodeForScene(sceneName: "taxi.dae", nodeName: "taxi") {
-//            taxiNode.position = SCNVector3(0,0,-0.8)
-//            taxiNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
-//            self.sceneView.scene.rootNode.addChildNode(taxiNode);
-//        }
-//    }
-	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		let configuration = ARWorldTrackingConfiguration()
@@ -100,7 +81,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		//track objects in ARWorld and start session
 		sceneView.session.run(configuration)
 	}
-// MARK: - ARSCNViewDelegate
+	//MARK: - ARSCNViewDelegate
 	func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
 		//if no anchor found, don't render anything!
 		if !(anchor is ARPlaneAnchor) {
@@ -115,7 +96,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 			let pyramidGeometry = SCNPyramid(width: CGFloat(plane.planeGeometry.width / 8), height: plane.planeGeometry.height / 8, length: plane.planeGeometry.height / 8)
 			pyramidGeometry.firstMaterial?.diffuse.contents = UIColor.white
 			let pyramidNode = SCNNode(geometry: pyramidGeometry)
+			pyramidNode.name = "pyramid"
 			pyramidNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+			pyramidNode.physicsBody?.categoryBitMask = BodyType.pyramid.rawValue | BodyType.plane.rawValue
+			pyramidNode.physicsBody?.contactTestBitMask = BodyType.box.rawValue
 			pyramidNode.position = SCNVector3(-(plane.planeGeometry.width) / 3, 0, plane.planeGeometry.height / 3)
 			node.addChildNode(pyramidNode)
 		}
@@ -131,6 +115,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         plane?.update(anchor: anchor as! ARPlaneAnchor)
     }
+	//MARK: SCNPhysicsContactDelegate
+	func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+		print("WE MADE CONTACT!!!!")
+	}
 	override func viewWillDisappear(_ animated: Bool) {
 		super.viewWillDisappear(animated)
 		//pause session
