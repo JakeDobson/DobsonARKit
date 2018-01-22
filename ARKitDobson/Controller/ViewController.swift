0514dad6af47bb594a4cd3fc97ffaeea5c522db0
Jake Dobson <jake.dobson@gmail.com>
2018-01-22 08:17:32 -0800

    init setup, just a scene and a box

--- ARKitDobson/Controller/ViewController.swift
+++ ARKitDobson/Controller/ViewController.swift
@@ -11,10 +11,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 	//globals
 	let configuration = ARWorldTrackingConfiguration()
 	let tapGestureRecognizer = UITapGestureRecognizer()
-	let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
-	let mat = SCNMaterial()
-	let boxNode = SCNNode()
-	let scene = SCNScene()
 	//life cycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -23,10 +19,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		// Show statistics such as fps and timing information
 		sceneView.showsStatistics = true
 		//form box with /mterial/dimensions/position
-		boxNode.position = SCNVector3(0, 0, -0.5)
-		boxNode.geometry = box
-		mat.diffuse.contents = UIColor.purple
-		box.firstMaterial = mat
+		let boxNode = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
 		//add boxNode to scene
 		scene.rootNode.addChildNode(boxNode)
 		//give target to gesture recognizer and call tapped func to move box
@@ -51,20 +44,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		let touchLocation = recognizer.location(in: sceneView)
 		//get hitTestResults
 		let hitResult = sceneView.hitTest(touchLocation, options: nil)
-		//if box touched, move box
-		if hitResult.isEmpty { //alert user to touch box?
-			print("pressed inside SELF.SCENEVIEW")
-		} else { //move box
-			boxNode.runAction(SCNAction.rotateBy(x: 1, y: 1, z: .pi * 2, duration: 4))
-		// below is an alternate spin method that runs forever \\
-//			boxNode.pivot = SCNMatrix4MakeRotation(.pi / 2, 1, 0, 0)
-//			let spin = CABasicAnimation(keyPath: "rotation")
-//			spin.fromValue = NSValue(scnVector4: SCNVector4(0, 0, 1, 0))
-//			spin.toValue = NSValue(scnVector4: SCNVector4(0, 0, 1, 6.28))
-//			spin.duration = 3
-//			spin.repeatCount = .infinity
-//			boxNode.addAnimation(spin, forKey: "spin around")
-		}
 	}
 // MARK: - ARSCNViewDelegate
 	/*
