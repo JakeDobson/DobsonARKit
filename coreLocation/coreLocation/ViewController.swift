3678741c3b593557b1faafc21a83aaf48a0ab07c
Josh Dobson <joshua@joshuarule.com>
2018-02-02 11:39:22 -0500

    creates coreLocation project and adds places view

---
+++ coreLocation/coreLocation/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  coreLocation
+//
+//  Created by Josh Dobson on 2/2/18.
+//  Copyright © 2018 Josh Dobson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var place :String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = self.place
+    }
+    
+
+}
+
