70702ef85972240860a0de9210c405ab7f06f53d
Josh Dobson <joshua@joshuarule.com>
2018-02-05 11:06:56 -0500

    adds placeAnnotation class

---
+++ coreLocation/coreLocation/PlaceAnnotation.swift
@@ -0,0 +1,28 @@
+//
+//  PlaceAnnotation.swift
+//  NeAR Me
+//
+//  Created by Josh Dobson on 2/5/18.
+//  Copyright © 2018 Josh Dobson. All rights reserved.
+//
+
+import Foundation
+import ARCL
+import CoreLocation
+import SceneKit
+
+class PlaceAnnotation : LocationNode {
+    var title :String!
+    var annotationNode :SCNNode
+    
+    init(location: CLLocation?, title: String) {
+        self.annotationNode = SCNNode()
+        super.init(location: location)
+        
+        self.title = title
+        //2:32
+    }
+}
+
+
+
