a565325036f27670e522a1e72818fb032fcf4132
Jake Dobson <jake.dobson@gmail.com>
2018-02-06 08:15:27 -0800

    added UIImagePickerController functionality and storyboard setup/linking

--- ARKitDobson/Controller/ViewController.swift
+++ ARKitDobson/Controller/ViewController.swift
@@ -5,36 +5,28 @@
 import UIKit
 import SceneKit
 import ARKit
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 	//outlets
-    @IBOutlet var sceneView: ARSCNView!
+	@IBOutlet weak var imgView: UIImageView!
 	//globals
-	let configuration = ARWorldTrackingConfiguration()
-	let tapGestureRecognizer = UITapGestureRecognizer()
-	let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
-	let mat = SCNMaterial()
-	let boxNode = SCNNode()
-	let scene = SCNScene()
+	private var imgPicker = UIImagePickerController()
 	//life cycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Set the view's delegate
-		sceneView.delegate = self
-		// Show statistics such as fps and timing information
-		sceneView.showsStatistics = true
-		//form box with /mterial/dimensions/position
-		boxNode.position = SCNVector3(0, 0, -0.5)
-		boxNode.geometry = box
-		mat.diffuse.contents = UIColor.purple
-		box.firstMaterial = mat
-		//add boxNode to scene
-		scene.rootNode.addChildNode(boxNode)
-		//give target to gesture recognizer and call tapped func to move box
-		tapGestureRecognizer.addTarget(self, action: #selector(tapped))
-		sceneView.addGestureRecognizer(tapGestureRecognizer)
-		// Set the scene to the view
-		sceneView.scene = scene
+		
+		self.imgPicker.sourceType = .photoLibrary
+		self.imgPicker.delegate = self
+		
 	}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		dismiss(animated: true, completion: nil)
+		guard let pickedImg = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+			return
+		}
+		self.imgView.image = pickedImg
+	}
+	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		//track objects in ARWorld and start session
@@ -46,26 +38,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		sceneView.session.pause()
 	}
 //MARK: helper funcs
-	@objc func tapped(recognizer: UITapGestureRecognizer) {
-		//get location of touch from user
-		let touchLocation = recognizer.location(in: sceneView)
-		//get hitTestResults
-		let hitResult = sceneView.hitTest(touchLocation, options: nil)
-		//if box touched, move box
-		if hitResult.isEmpty { //alert user to touch box?
-			print("pressed inside SELF.SCENEVIEW")
-		} else { //move box
-			boxNode.runAction(SCNAction.rotateBy(x: 1, y: 1, z: .pi * 2, duration: 4))
-		// below is an alternate spin method that runs forever \\
-//			boxNode.pivot = SCNMatrix4MakeRotation(.pi / 2, 1, 0, 0)
-//			let spin = CABasicAnimation(keyPath: "rotation")
-//			spin.fromValue = NSValue(scnVector4: SCNVector4(0, 0, 1, 0))
-//			spin.toValue = NSValue(scnVector4: SCNVector4(0, 0, 1, 6.28))
-//			spin.duration = 3
-//			spin.repeatCount = .infinity
-//			boxNode.addAnimation(spin, forKey: "spin around")
-		}
+
+//MARK: actions
+	@IBAction func openPhotoLibraryButtonPressed(_ sender: Any) {
+		self.present(self.imgPicker, animated: true, completion: nil)
 	}
+	
+
 // MARK: - ARSCNViewDelegate
 	/*
 	// Override to create and configure nodes for anchors added to the view's session.
